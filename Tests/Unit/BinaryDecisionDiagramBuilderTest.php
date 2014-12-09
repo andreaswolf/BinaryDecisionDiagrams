652a8aeb66a685f0f444bcc1518159999fef523e
Andreas Wolf <aw@foundata.net>
2014-12-09 15:10:19 +0100

    [TASK] Add more comoplicated function as testcase

--- Tests/Unit/BinaryDecisionDiagramBuilderTest.php
+++ Tests/Unit/BinaryDecisionDiagramBuilderTest.php
@@ -87,4 +87,25 @@ class BinaryDecisionDiagramBuilderTest extends \PHPUnit_Framework_TestCase {
 		$this->assertTrue($diagram->getResultForInput(array('A' => FALSE, 'B' => TRUE)));
 	}
 
+	/**
+	 * @test
+	 */
+	public function functionWithOrsNestedInAndIsCorrectlyBuilt() {
+		// (A || B) && (C || D)
+		$subject = new BinaryDecisionDiagramBuilder(array('A', 'B', 'C', 'D'));
+
+		$subject->addAllowedInput(array('A' => FALSE, 'B' => TRUE, 'C' => FALSE, 'D' => TRUE), TRUE); // FTFT
+		$subject->addAllowedInput(array('A' => FALSE, 'B' => TRUE, 'C' => TRUE), TRUE); // FTT-
+		$subject->addAllowedInput(array('A' => TRUE, 'C' => FALSE, 'D' => TRUE), TRUE); // T-FT
+		$subject->addAllowedInput(array('A' => TRUE, 'C' => TRUE), TRUE); // T-T-
+
+		$diagram = $subject->getDiagram();
+
+		$this->assertFalse($diagram->getResultForInput(array('A' => FALSE, 'B' => FALSE, 'C' => TRUE, 'D' => FALSE)));
+		$this->assertFalse($diagram->getResultForInput(array('A' => TRUE, 'B' => FALSE, 'C' => FALSE, 'D' => FALSE)));
+		$this->assertTrue($diagram->getResultForInput(array('A' => TRUE, 'B' => FALSE, 'C' => FALSE, 'D' => TRUE)));
+		$this->assertTrue($diagram->getResultForInput(array('A' => TRUE, 'B' => FALSE, 'C' => TRUE, 'D' => FALSE)));
+		$this->assertTrue($diagram->getResultForInput(array('A' => TRUE, 'B' => TRUE, 'C' => TRUE, 'D' => TRUE)));
+	}
+
 }
